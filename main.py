6ffab3d9112246edf7db74410c6fe0b5ed91696d
Santosh Narayanan <santosh33@gmail.com>
2025-08-31 13:34:29 +0200

    add code for reflexion agent

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 def main():
     print("Hello from ai-reflexion!")
+    print(os.environ.get("OPENAI_API_KEY"))
 
 
 if __name__ == "__main__":
